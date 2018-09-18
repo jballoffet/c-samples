39581661767b8b46a584901b2bd9d63e00870922
JavierBalloffet <javier.balloffet@gmail.com>
2018-09-17 22:45:38 -0300

    Added two examples in chapter 28

---
+++ 28. Archivos de Texto/ejemplo2/main.c
@@ -0,0 +1,32 @@
+/**
+ * \file            main.c
+ * \brief           28. Archivos de Texto - Ejemplo 2
+ * \author          Javier Balloffet
+ * \date            17-SEP-2018
+ * \details         Usar makefile para compilar, linkear y ejecutar
+ */
+
+#include <stdio.h>
+
+int main() {
+    FILE* fp;
+    char fileLine[60];
+	
+    // 1. Abro el archivo en modo lectura (r)
+    fp = fopen("archivo.txt", "r");
+
+    // 2. Chequeo si hubo un error al abrir el archivo
+    if (fp == NULL) {
+        printf("Error abriendo el archivo!\n");
+        return -1;
+    }
+
+    // 3. Leo e imprimo en pantalla la primera linea del archivo
+    fgets(fileLine, 60, fp);
+    printf("%s", fileLine);
+
+    // 4. Cierro el archivo
+    fclose(fp);
+
+	return 0;
+}
