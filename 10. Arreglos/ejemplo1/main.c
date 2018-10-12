ced5956cb7155b0fc63fc70bb7ac102895b51b8e
JavierBalloffet <javier.balloffet@gmail.com>
2018-10-12 17:47:18 -0300

    Added chapter 10

---
+++ 10. Arreglos/ejemplo1/main.c
@@ -0,0 +1,36 @@
+/**
+ * \file            main.c
+ * \brief           10. Arreglos - Ejemplo 1 - Declaración de arrays
+ * \author          Javier Balloffet
+ * \date            12-OCT-2018
+ * \details         Usar makefile para compilar, linkear y ejecutar
+ */
+
+#include <stdio.h>
+
+int main() {
+    // 1. Declaro un arreglo (array) de enteros (int) de 5 posiciones
+    int array[5];
+    
+    // 2. Inicializo el arreglo
+    array[0] = 3;
+    array[1] = 24;
+    array[2] = 12;
+    array[3] = 5;
+    array[4] = 76;
+
+    // 3. Imprimo el contenido del arreglo
+    printf("Valor de array[0] = %d\n", array[0]);
+    printf("Valor de array[1] = %d\n", array[1]);
+    printf("Valor de array[2] = %d\n", array[2]);
+    printf("Valor de array[3] = %d\n", array[3]);
+    printf("Valor de array[4] = %d\n", array[4]);
+
+    // 4. Imprimo el tamaño del arreglo (en bytes)
+    printf("Tamanio del arreglo (en bytes) = %ld\n", sizeof(array));
+
+    // 5. Imprimo el tamaño del arreglo (en posiciones)
+    printf("Tamanio del arreglo (en posiciones) = %ld\n", sizeof(array) / sizeof(int));
+
+	return 0;
+}
---
+++ 9. Funciones - Argumentos por Referencia/ejemplo2/main.c
@@ -0,0 +1,32 @@
+/**
+ * \file            main.c
+ * \brief           9. Funciones - Argumentos por Referencia - Ejemplo 2 - Swap variables
+ * \author          Javier Balloffet
+ * \date            12-OCT-2018
+ * \details         Usar makefile para compilar, linkear y ejecutar
+ */
+
+#include <stdio.h>
+
+void swap(int* number_p1, int* number_p2);
+
+int main() {
+    int number1 = 5, number2 = 10;
+
+    // 1. Invoco a "swap", función que intercambia el contenido de dos variables
+    printf("Valor de \"number1\" antes del intercambio = %d\n", number1);
+    printf("Valor de \"number2\" antes del intercambio = %d\n", number2);
+    swap(&number1, &number2);
+    printf("Valor de \"number1\" despues del intercambio = %d\n", number1);
+    printf("Valor de \"number2\" despues del intercambio = %d\n", number2);
+
+	return 0;
+}
+
+void swap(int* number_p1, int* number_p2) {
+    // 2. Intercambio el contenido de las variables
+    int aux;
+    aux = *number_p1;
+    *number_p1 = *number_p2;
+    *number_p2 = aux;
+}
