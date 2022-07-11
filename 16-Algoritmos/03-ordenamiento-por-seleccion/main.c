f6cd6995c9ed4823e4daea28339ac1b15c4a4338
Javier Balloffet <javier.balloffet@gmail.com>
2022-07-10 22:56:51 -0300

    Change examples order in unit 16

--- 16-Algoritmos/03-ordenamiento-por-seleccion/main.c
+++ 16-Algoritmos/03-ordenamiento-por-seleccion/main.c
@@ -1,15 +1,12 @@
 /*!
  * @file   main.c
- * @brief  16. Algoritmos - 03. Ordenamiento de Burbuja (Bubble Sort)
+ * @brief  16. Algoritmos - 03. Ordenamiento por Selección (Selection Sort)
  * @author Javier Balloffet <javier.balloffet@gmail.com>
  * @date   Sep 7, 2018
  */
 #include <stdio.h>
 
-#define FALSE 0
-#define TRUE 1
-
-void bubble_sort(int* array, int length);
+void selection_sort(int* array, int length);
 void print_array(const int* array, int length);
 void swap(int* x, int* y);
 
@@ -20,9 +17,9 @@ int main(void)
     printf("Contenido del arreglo (original): ");
     print_array(array, 8);
 
-    // Ordeno el arreglo utilizando el método de Ordenamiento de Burbuja (Bubble
-    // Sort).
-    bubble_sort(array, 8);
+    // Ordeno el arreglo utilizando el método de Ordenamiento por Selección
+    // (Selection Sort).
+    selection_sort(array, 8);
 
     printf("Contenido del arreglo (obtenido): ");
     print_array(array, 8);
@@ -30,27 +27,26 @@ int main(void)
     return 0;
 }
 
-void bubble_sort(int* array, int length)
+void selection_sort(int* array, int length)
 {
-    int i, j, swapped;
+    int i, j, min_index;
 
+    // El límite del arreglo desordenado se corre uno a uno.
     for (i = 0; i < length - 1; i++)
     {
-        swapped = FALSE;
-        for (j = 0; j < length - i - 1; j++)
+        // Encuentro el menor elemento en el arreglo desordenado.
+        min_index = i;
+        for (j = i + 1; j < length; j++)
         {
-            if (array[j] > array[j + 1])
+            if (array[j] < array[min_index])
             {
-                swap(&array[j], &array[j + 1]);
-                swapped = TRUE;
+                min_index = j;
             }
         }
 
-        // Si no intercambió nunca, entonces el arreglo ya está ordenado!
-        if (swapped == FALSE)
-        {
-            break;
-        }
+        // Intercambio el mínimo elemento encontrado con el primer elemento del
+        // arreglo desordenado.
+        swap(&array[min_index], &array[i]);
     }
 }
 
--- 16-Algoritmos/04-ordenamiento-de-burbuja/main.c
+++ 16-Algoritmos/04-ordenamiento-de-burbuja/main.c
@@ -1,12 +1,15 @@
 /*!
  * @file   main.c
- * @brief  16. Algoritmos - 04. Ordenamiento por Selección (Selection Sort)
+ * @brief  16. Algoritmos - 04. Ordenamiento de Burbuja (Bubble Sort)
  * @author Javier Balloffet <javier.balloffet@gmail.com>
  * @date   Sep 7, 2018
  */
 #include <stdio.h>
 
-void selection_sort(int* array, int length);
+#define FALSE 0
+#define TRUE 1
+
+void bubble_sort(int* array, int length);
 void print_array(const int* array, int length);
 void swap(int* x, int* y);
 
@@ -17,9 +20,9 @@ int main(void)
     printf("Contenido del arreglo (original): ");
     print_array(array, 8);
 
-    // Ordeno el arreglo utilizando el método de Ordenamiento por Selección
-    // (Selection Sort).
-    selection_sort(array, 8);
+    // Ordeno el arreglo utilizando el método de Ordenamiento de Burbuja (Bubble
+    // Sort).
+    bubble_sort(array, 8);
 
     printf("Contenido del arreglo (obtenido): ");
     print_array(array, 8);
@@ -27,26 +30,27 @@ int main(void)
     return 0;
 }
 
-void selection_sort(int* array, int length)
+void bubble_sort(int* array, int length)
 {
-    int i, j, min_index;
+    int i, j, swapped;
 
-    // El límite del arreglo desordenado se corre uno a uno.
     for (i = 0; i < length - 1; i++)
     {
-        // Encuentro el menor elemento en el arreglo desordenado.
-        min_index = i;
-        for (j = i + 1; j < length; j++)
+        swapped = FALSE;
+        for (j = 0; j < length - i - 1; j++)
         {
-            if (array[j] < array[min_index])
+            if (array[j] > array[j + 1])
             {
-                min_index = j;
+                swap(&array[j], &array[j + 1]);
+                swapped = TRUE;
             }
         }
 
-        // Intercambio el mínimo elemento encontrado con el primer elemento del
-        // arreglo desordenado.
-        swap(&array[min_index], &array[i]);
+        // Si no intercambió nunca, entonces el arreglo ya está ordenado!
+        if (swapped == FALSE)
+        {
+            break;
+        }
     }
 }
 
