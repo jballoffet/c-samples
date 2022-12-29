e32aebd68dd9afc39958a48e519b2b96e6003b43
Javier Balloffet <javier.balloffet@gmail.com>
2022-12-29 15:46:00 -0300

    Add unit 27 examples

--- 27-System-calls-de-Entrada-y-Salida/ejemplo02/main.c
+++ 27-System-calls-de-Entrada-y-Salida/ejemplo02/main.c
@@ -1,7 +1,7 @@
 /*!
  * @file   main.c
- * @brief  27. System calls de Entrada y Salida - 01. Escribiendo un archivo
- *   usando system calls
+ * @brief  27. System calls de Entrada y Salida - 02. Leyendo un archivo usando
+ *   system calls
  * @author Javier Balloffet <javier.balloffet@gmail.com>
  * @date   Dec 29, 2022
  */
@@ -14,12 +14,12 @@
 
 int main(void)
 {
-    // Declaro un entero para almacenar el descriptor del archivo.
     int fd;
-    char message[] = "Escribiendo un archivo usando system calls!\n";
+    char message[60];
+    int bytes_read = 0;
 
-    // Abro el archivo en modo sólo escritura. Si no existe, el mismo es creado.
-    fd = open("archivo.txt", O_WRONLY | O_CREAT);
+    // Abro el archivo en modo sólo lectura.
+    fd = open("archivo.txt", O_RDONLY);
 
     // Chequeo si hubo un error al abrir el archivo.
     if (fd == -1)
@@ -28,8 +28,10 @@ int main(void)
         return -1;
     }
 
-    // Escribo el archivo.
-    write(fd, message, strlen(message));
+    // Leo el archivo e imprimo en pantalla el mensaje obtenido.
+    bytes_read = read(fd, message, 60);
+    message[bytes_read] = '\0';
+    printf("%s", message);
 
     // Cierro el archivo.
     close(fd);
