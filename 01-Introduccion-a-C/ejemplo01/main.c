1bbcf652102906cfd6352a3453b5153e74d6cca9
Javier Balloffet <javier.balloffet@gmail.com>
2023-04-23 22:24:06 -0300

    Rename main file and remove unneeded README files

--- 01-Introduccion-a-C/ejemplo01/main.c
+++ 01-Introduccion-a-C/ejemplo01/main.c
@@ -1,5 +1,5 @@
 /*!
- * @file   hola.c
+ * @file   main.c
  * @brief  01. Introduccion a C - 01. Hola mundo
  * @author Javier Balloffet <javier.balloffet@gmail.com>
  * @date   Sep 7, 2018
@@ -9,7 +9,7 @@
 int main(void)
 {
     // Imprimo un mensaje de saludo en la pantalla.
-    printf("Hola mundo\n");
+    printf("¡Hola mundo!\n");
 
     return 0;
 }
